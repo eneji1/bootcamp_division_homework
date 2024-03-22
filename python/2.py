9ec236c30e5a5890c6fa90fcbba8330edd332b3b
eneji1 <ieunji828@naver.com>
2024-03-22 22:07:29 +0900

    해달 채점

--- python/2.py
+++ python/2.py
@@ -7,7 +7,8 @@ def main():
     # 이곳에 코드를 작성해주세요!
     ages = int(input())
     if ages >=61:
-        print('DISCOUNT \nBOARD')
+        print('DISCOUNT')
+        print('BORAD')
     else:
         print('BOARD')
     return
